d4c96be7ef8d34ce83c81831b0cf6cab2cb1f140
Ayenewtarekegn18 <101360057+Ayenewtarekegn18@users.noreply.github.com>
2023-01-09 01:08:14 +0300

    Create Maximum Ice cream bars.py

---
+++ Maximum Ice cream bars.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        count=0
+        i=0
+        while i < len(costs):
+            if costs[i]>coins:
+                 break
+            coins-=costs[i]
+            count=count+1
+               
+            i=i+1
+        return count
