17b9be88b960e93538aeb1d9abad491e6a3febce
Ayenewtarekegn18 <101360057+Ayenewtarekegn18@users.noreply.github.com>
2023-01-12 17:18:21 +0300

    Create valid parenthesis.py

---
+++ valid parenthesis.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        i=0
+        count=0
+        stack=[]
+        while i < len(s):
+            if s[i]=='(' or s[i]=='[' or s[i]=='{' :         
+                stack.append(s[i])
+                i=i+1
+            else: 
+                if s[i]==')' :
+                    if len(stack)!=0 and stack[len(stack)-1]=='(':
+                        stack.pop()
+                        i=i+1
+                    else:
+                        return False
+                elif s[i]==']': 
+                    if len(stack)!=0 and stack[len(stack)-1]=='[':
+                        stack.pop()
+                        i=i+1
+                    else:
+                        return False
+                elif s[i]=='}' :
+                    if len(stack)!=0 and stack[len(stack)-1]=='{':
+                        stack.pop()
+                        i=i+1
+                    else:
+                        return False
+                
+        if len(stack)==0:
+            return True
+        else:
+            return False
