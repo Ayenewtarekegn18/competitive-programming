bae18d28bcf858724952092ae85aa5af0e1f4145
Ayenewtarekegn18 <101360057+Ayenewtarekegn18@users.noreply.github.com>
2023-03-02 19:48:18 +0300

    Update Check If N and Its Double Exist.cpp

--- Check If N and Its Double Exist.cpp
+++ Check If N and Its Double Exist.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         set <int> x;
-        int count=0;
-        for(auto i:arr){
+        for(auto i:arr)
+        {
             x.insert(i);
         }
-          for(auto i:arr)
-          {
-          if (i==0)
-          count++;
-          }
-        if (count>=2)
-          return true;
-
-        for(auto i:arr){
+        for(auto i:arr)
+        {
             if (i!=0 && x.find(i*2)!=x.end())
              return true;
         }
        
-        return false;
+        return count(arr.begin(),arr.end(),0)>1;
     }
 };
