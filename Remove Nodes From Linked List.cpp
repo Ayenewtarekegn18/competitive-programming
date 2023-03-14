1aae9daaf14d27004caa74c963142c22598f24bd
Ayenewtarekegn18 <101360057+Ayenewtarekegn18@users.noreply.github.com>
2023-03-15 00:06:37 +0300

    Create Remove Nodes From Linked List.cpp

---
+++ Remove Nodes From Linked List.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+ListNode* removeNodes(ListNode* head) {
+        vector<int> arr;
+        ListNode *p=head,*q=nullptr;
+        while(p)
+        {
+            arr.push_back(p->val);
+            p=p->next;
+        }
+        stack<pair<int,int>> st; 
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+            if(st.empty())
+            {
+                st.push({arr[i],1});
+            }
+            else
+            {
+                if(st.top().first>arr[i])
+                { 
+                    st.push({st.top().first,-1});
+                }
+                else
+                {
+                    st.push({arr[i],1}); 
+                }
+            }
+        }
+        p=head;
+        while(st.size())
+        {
+            if(st.top().second==1)
+            {
+                p->val = st.top().first;
+                q=p;
+                p=p->next;
+            }
+            st.pop();
+        }
+        q->next = nullptr;
+        return head;
+    }
+};
