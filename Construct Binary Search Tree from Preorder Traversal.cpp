3d498129fda29c0ca4f14b3351d37c6a476c5d35
Ayenewtarekegn18 <101360057+Ayenewtarekegn18@users.noreply.github.com>
2023-03-27 23:26:41 +0300

    Create Construct Binary Search Tree from Preorder Traversal.cpp

---
+++ Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+ 
+
+    void insert(TreeNode* & root,int x){
+        if(root==NULL){
+            root= new TreeNode(x);
+            return ;
+        }
+        else if(root->val<x){
+          insert(root->right,x);
+        }
+        else if(root->val>x){
+            insert(root->left,x);
+        }
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        
+        TreeNode* root=new TreeNode(preorder[0]);
+        TreeNode* temp=root;
+        for(int i=1;i<preorder.size();i++){
+            insert(temp,preorder[i]);
+        }
+        return root;
+    }
+};
