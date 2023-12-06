225b956ecb06964fb5bc386c1172671b5b170c79
Ayenew Tarekegn <101360057+Ayenewtarekegn18@users.noreply.github.com>
2023-12-06 18:34:57 +0300

    Added solution for Calculate Money in Leetcode Bank

---
+++ week-2/python/calculate-money-in-leetcode-bank.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        i=1
+        num = 8
+        j=1
+        count=1
+        sum=0
+        for i in range(1,n+1):
+            if i==num:
+                num+=7
+                count+=1
+                j=count
+            sum+=j
+            j+=1
+        return sum
+        
+
