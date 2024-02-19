6f509a4a63851d712d778f194598d0e389d33661
Ayenew Tarekegn <101360057+Ayenewtarekegn18@users.noreply.github.com>
2024-02-19 17:03:16 +0300

    Add solution for Maximum Subarray

---
+++ pre-camp/week/leetcode/maximum-subarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxsum = nums[0]
+        curr = 0
+
+        for i in nums:
+            if curr < 0:
+                curr = 0
+            curr += i
+            maxsum = max(maxsum,curr)
+        return maxsum
