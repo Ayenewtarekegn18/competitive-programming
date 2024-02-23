eb112d1e478cc5cf74c3396b4bc5299f8302248f
Ayenew Tarekegn <101360057+Ayenewtarekegn18@users.noreply.github.com>
2024-02-23 19:53:37 +0300

    Add solution for Sliding Window Maximum

---
+++ pre-camp/week/leetcode/sliding-window-maximum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        mono = deque()
+        ans = []
+        r = 0
+        for l in range(len(nums)-k+1):
+            while r-l+1 <= k:
+                while mono and mono[-1] < nums[r]:
+                    mono.pop()
+                mono.append(nums[r])
+                r+=1
+            ans.append(mono[0])
+            if nums[l] == mono[0]:
+                mono.popleft()
+        return ans
+            
+                
+            
+                
+
