254c6615b2bce989071a809d598217b6c196ddd6
Ayenew Tarekegn <101360057+Ayenewtarekegn18@users.noreply.github.com>
2024-02-26 19:20:07 +0300

    Add solution for Pascal's Triangle II

---
+++ pre-camp/week/leetcode/pascals-triangle-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        prevArr = self.getRow(rowIndex-1)
+        currArr = [1] * (rowIndex+1)
+        for i in range(1,rowIndex):
+            currArr[i] = prevArr[i] + prevArr[i-1]
+        return currArr        
