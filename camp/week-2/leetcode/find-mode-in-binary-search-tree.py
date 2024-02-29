6cc44f57bdff89bde920d81b6cfca40a7e62f6c7
Ayenew Tarekegn <101360057+Ayenewtarekegn18@users.noreply.github.com>
2024-02-29 15:25:08 +0300

    Add solution for Find Mode in Binary Search Tree

---
+++ camp/week-2/leetcode/find-mode-in-binary-search-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        freq = defaultdict(int)
+        stack = []
+        def traverse(root):
+            if not root :
+                return 
+            freq[root.val] += 1
+            traverse(root.left)
+            traverse(root.right)
+        traverse(root)
+      
+        for key,val in freq.items():
+            while stack and freq[stack[-1]] < val:
+                stack.pop()
+            if not stack or freq[stack[-1]] == val :
+                stack.append(key)
+        
+        return stack
