7110653635cb2d67117da9b35249f6a76a6db6f3
Ayenew Tarekegn <101360057+Ayenewtarekegn18@users.noreply.github.com>
2024-02-29 15:00:51 +0300

    Add solution for Count Good Numbers

---
+++ camp/week-2/leetcode/count-good-numbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        def power(x,y):
+            if y ==1:
+                return x
+            if y == 0:
+                return 1
+            num = power(x,y//2)
+            if y % 2 == 0:
+                return (num *num) % (10**9 +7) 
+            else:
+                return (num *num * x) % (10**9 +7)  
+
+        even = n//2
+        odd = n //2 + n % 2
+        return (power(5,odd) * power(4,even)) % (10**9 + 7)
